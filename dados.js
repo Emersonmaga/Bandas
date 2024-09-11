ceaad80ae48d9fc1a42332bcfa81f2c4f1289f99
Emersonmaga <emerson-maga@hotmail.com>
2024-09-10 21:36:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+   { 
+    titulo:"Rayssa Leal",
+    descricao:"Rayssa Leal, a Fadinha, é uma das maiores promessas do skate mundial.Com apenas 13 anos de idade, ela já conquistou diversos títulos e se tornou um ícone para a nova geração de skatistas",
+    link: "https://en.wikipedia.org/wiki/Rayssa_Leal"
+   },
+
+   {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma talentosa jogadora de [jogo], conhecida por [suas habilidades].",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+},
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica e mundial.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+}
+]
+
